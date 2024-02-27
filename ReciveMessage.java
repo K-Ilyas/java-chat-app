2e1630c55701ff7e06c0c21424af9e032d924a46
Ahmed Guetti <guettiahmed101@gmail.com>
2024-02-27 02:50:22 +0100

    [DIRECT] the code created locally and pushed at once

--- ReciveMessage.java
+++ ReciveMessage.java
@@ -1,3 +1,55 @@
-public class ReciveMessage{
-    
+import java.net.Socket;
+import java.io.DataInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class ReciveMessage implements Runnable {
+
+    Socket soc = null;
+    UserInformation user = null;
+
+    public ReciveMessage(Socket soc, UserInformation user) {
+        this.soc = soc;
+        this.user = user;
+    }
+
+    public void run() {
+        String sender = "", time = "", data = "";
+        int header = 1;
+        try {
+            InputStream in = this.soc.getInputStream();
+            DataInputStream dis = new DataInputStream(in);
+            do {
+                API.printMessage("[" + user.getPseudo() + "] : Waiting....");
+                API.printMessage("\nEnter the user pseudo or nothing to brodcast ===>> ");
+
+                header = dis.readInt();
+                if (header == 1) {
+                    sender = dis.readUTF();
+                    time = dis.readUTF();
+                    data = dis.readUTF();
+
+
+                    API.printMessage("\n\n{ TIME : [" + time + "] FROM : [" + sender + "]  }" + "\n{ MESSAGE: [" + data
+                            + "]  }\n\n");
+                } else if (header == 2) {
+
+                    API.printMessage(dis.readUTF());
+
+
+                } else {
+                    API.printMessage(dis.readUTF());
+                }
+
+            } while (header != 0);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
--- UserInformation.java
+++ UserInformation.java
@@ -1,3 +1,41 @@
-public class UserInformation{
-    
+import java.io.Serializable;
+import java.lang.Comparable;
+
+public class UserInformation implements Comparable<UserInformation>, Serializable {
+
+    private String pseudo = "";
+    private String password = "";
+
+    public UserInformation(String pseudo, String password) {
+
+        this.pseudo = pseudo;
+        this.password = password;
+
+    }
+
+    public int compareTo(UserInformation o) {
+
+        if (o.getClass().equals(UserInformation.class)) {
+            UserInformation cd = (UserInformation) o;
+            return this.pseudo.compareTo(cd.getPassword()) & this.password.compareTo(cd.getPassword());
+        }
+        return -1;
+    }
+
+    public String getPseudo() {
+        return this.pseudo;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPesudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
